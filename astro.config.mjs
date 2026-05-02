237fe48f7b0ce2139ed32866cd64175ac2c4e679
Josh Pullen <JoshPullen27@gmail.com>
2026-05-02 09:33:12 -0400

    Better form redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-})
+  redirects: {
+    "/form":
+      "https://docs.google.com/forms/d/e/1FAIpQLSe0axLrX6rW9X-Og9-UCs_NVkcq1M66opaFRXuF8yu3M3JRBw/viewform?usp=dialog",
+  },
+});

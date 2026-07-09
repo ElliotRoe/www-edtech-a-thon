dae1d1787a3535e6682fbc96265d157b28aa50cd
Elliot Roe <elliotbroe@gmail.com>
2026-07-09 13:09:17 -0400

    feat: added zoom link

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ export default defineConfig({
       "https://docs.google.com/forms/d/e/1FAIpQLSe0axLrX6rW9X-Og9-UCs_NVkcq1M66opaFRXuF8yu3M3JRBw/viewform?usp=dialog",
     "/dev":
       "https://docs.google.com/forms/d/e/1FAIpQLSeM-xSUAg1bBx2RgfHPwO_GQCiwu3ZB2FKG-7PeGyoaBSa6gQ/viewform?usp=dialog",
+    "/join":
+      "https://us05web.zoom.us/j/7767707462?pwd=hCfz9pYC591EGeTHl1gmgK2ra0RzWE.1",
   },
 
   integrations: [markdoc(), svelte()],
